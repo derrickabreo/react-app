d0a6947839443df2dfd58ced016cd0a8b828f1d9
Derrick <derrickabreo@gmail.com>
2021-10-13 21:02:25 +0100

    React setup

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View , Button} from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>You clicked me {outputText} times!</Text>
+      <Button title="Click me" onPress={() => setOutputText(outputText+1)}/>
     </View>
   );
 }
